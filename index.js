9a391550822214ab40286782ba07ae77ee410cdd
Davidb-2107 <david.b2107@gmail.com>
2022-06-23 18:05:04 +0200

    added EL

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-let addPoints = document.querySelectorAll(".add-points");
-console.log(addPoints)
+const button1 = document.querySelectorAll(".add1");
+const button2 = document.querySelectorAll(".add2");
+const button3 = document.querySelectorAll(".add3");
+
+button1.forEach(button1 => button1.addEventListener('click',
+addNPoints => {
+    
+    console.log("clicked");
+}));
+
+button2.forEach(button2 => button2.addEventListener('click',
+addNPoints => {
+    
+    console.log("clicked");
+}));
+
+button3.forEach(button3 => button3.addEventListener('click',
+addNPoints => {
+    
+    console.log("clicked");
+}));
+
+
