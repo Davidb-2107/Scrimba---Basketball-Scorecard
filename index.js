7b4dc93e9f69af2ea7548fcbb84b7ead3e24d6d0
Davidb-2107 <david.b2107@gmail.com>
2022-06-23 18:54:46 +0200

    completed guest part

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-const button1 = document.querySelectorAll(".add1");
-const button2 = document.querySelectorAll(".add2");
-const button3 = document.querySelectorAll(".add3");
 
-button1.forEach(button1 => button1.addEventListener('click',
-addNPoints => {
-    
-    console.log("clicked");
+
+
+    //#SELECT elements//
+//##SELECT buttons
+    //###Home buttons
+const buttonH1 = document.querySelectorAll("button.home.add1");
+const buttonH2 = document.querySelectorAll("button.home.add2");
+const buttonH3 = document.querySelectorAll("button.home.add3");
+    //###Guest buttons
+const buttonG1 = document.querySelectorAll("button.guest.add1");
+const buttonG2 = document.querySelectorAll("button.guest.add2");
+const buttonG3 = document.querySelectorAll("button.guest.add3");
+
+
+//##SELECT displayed score
+const displayPointsH = document.querySelector(".display_points_text.H");
+const displayPointsG = document.querySelector(".display_points_text.G");
+
+
+//#GLOBAL variable
+let scoreG = 0;
+let scoreH = 0;
+
+
+
+buttonH1.forEach(buttonH1 => buttonH1.addEventListener('click',() => {
+     scoreH ++ ;
+     displayPointsH.textContent = scoreH;
+ }));
+
+ buttonH2.forEach(buttonH2 => buttonH2.addEventListener('click',() => {
+     console.log(buttonH2)
+     scoreH += 2;
+     displayPointsH.textContent = scoreH;
+ }));
+
+buttonH3.forEach(buttonH3 => buttonH3.addEventListener('click',() => {
+    scoreH += 3;
+    displayPointsH.textContent = scoreH;
 }));
 
-button2.forEach(button2 => button2.addEventListener('click',
-addNPoints => {
-    
-    console.log("clicked");
-}));
 
-button3.forEach(button3 => button3.addEventListener('click',
-addNPoints => {
-    
-    console.log("clicked");
-}));
+ buttonG1.forEach(buttonG1 => buttonG1.addEventListener('click',() => {
+     scoreG ++ ;
+     displayPointsG.textContent = scoreG;
+ }));
+
+ buttonG2.forEach(buttonG2 => buttonG2.addEventListener('click',() => {
+     scoreG += 2;
+     displayPointsG.textContent = scoreG;
+ }));
+
+buttonG3.forEach(buttonG3 => buttonG3.addEventListener('click',() => {
+     scoreG += 3;
+     displayPointsG.textContent = scoreG;
+ }));
+
+
+
 
 
